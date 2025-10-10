6da2f2987cfd3fb8568222a2ea1a3732865330d8
Luis <>
2025-10-10 20:17:23 -0300

    finalversion

---
+++ EDIII_Trabalho1/auxiliares_busca.c
@@ -0,0 +1,153 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "auxiliares_busca.h"
+#define MAXIMO 500
+
+RegistroPessoa *le_registro(FILE *arq)
+{
+    // Le e o status do arquivo e seu tamanho e guarda os resultados em um buffer
+    char buffer1;
+    int buffer2;
+    fread(&buffer1, sizeof(char), 1, arq);
+    fread(&buffer2, sizeof(int), 1, arq);
+
+    // Declara o ponteiro para o registro atual com um espaço na memória igual ao tamanho do registro +
+    // o status(1 byte) o tamanho(4 bytes) e os 2 \0 do nome e usuário (2 bytes) = tamanhoregistro + 7 byte 
+
+    RegistroPessoa *registro_atual = (RegistroPessoa*) malloc(buffer2 + 7);
+    registro_atual->removido = buffer1;
+    registro_atual->tamanhoRegistro = buffer2;
+
+    // So o registro atual foi removido retorna o registro_atual como está
+    if (registro_atual->removido == '1')
+    {
+        return registro_atual;
+    }
+
+    // Lê id idade e tamanhoNome
+    fread(&registro_atual->idPessoa, sizeof(int), 1, arq);
+    fread(&registro_atual->idadePessoa, sizeof(int), 1, arq);
+    fread(&registro_atual->tamanhoNomePessoa, sizeof(int), 1, arq);
+
+    // Aloca espaço para o nome = tamanhoNome + (1byte) do \0
+    registro_atual->nomePessoa = malloc(sizeof(char) * (registro_atual->tamanhoNomePessoa + 1));
+
+
+    // Se não houver nome copia "-" para o registro_atual
+    if (registro_atual->tamanhoNomePessoa == 0)
+    {
+        strcpy(registro_atual->nomePessoa, "-");
+        registro_atual->nomePessoa[1] = '\0';
+    }
+
+    //Do contrário copia o nome do arquivo para o registro_atual
+    else
+    {
+        fread(registro_atual->nomePessoa, sizeof(char), registro_atual->tamanhoNomePessoa, arq);
+        registro_atual->nomePessoa[registro_atual->tamanhoNomePessoa] = '\0';
+    }
+    fread(&registro_atual->tamanhoNomeUsuario, sizeof(int), 1, arq);
+    
+    // Aloca espaço para o nomeusuario = tamanhoNomeUsuario + (1byte) do \0
+    registro_atual->nomeUsuario = malloc(sizeof(char) * (registro_atual->tamanhoNomeUsuario + 1));
+
+     // Se não houver usuario copia "-" para o registro_atual
+    if (registro_atual->tamanhoNomeUsuario == 0)
+    {
+        strcpy(registro_atual->nomeUsuario, "-");
+        registro_atual->nomeUsuario[1] = '\0';
+    }
+    //Do contrário copia o nome de usuario do arquivo para o registro_atual
+    else
+    {
+        fread(registro_atual->nomeUsuario, sizeof(char), registro_atual->tamanhoNomeUsuario, arq);
+        registro_atual->nomeUsuario[registro_atual->tamanhoNomeUsuario] = '\0';
+    }
+    // Retorna o registro atual
+    return registro_atual;
+}
+
+void imprimir_saida(RegistroPessoa *registro_atual)
+{
+    // Imprime o registro_atual segundo as especificações do trabalho
+    printf("Dados da pessoa de codigo %d\n", registro_atual->idPessoa);
+    printf("Nome: %s\n", registro_atual->nomePessoa);
+    // Se a idade do registro_atual for -1 imprime "-" do contrário imprime a idade da memória
+    if (registro_atual->idadePessoa != -1)
+        printf("Idade: %d\n", registro_atual->idadePessoa);
+    else
+        printf("Idade: -\n");
+    printf("Usuario: %s\n\n", registro_atual->nomeUsuario);
+}
+
+void erro_abertura()
+{
+    //Imprime o erro de abertura do arquivo
+    printf("Falha no processamento do arquivo.\n");
+}
+
+void erro_registro()
+{
+    //Imprime o erro de registro
+    printf("Registro inexistente.\n");
+}
+
+CabecalhoPessoa* le_cabecalho(FILE *arq)
+{
+    // Le o cabeçalho do arquivo de dados
+    CabecalhoPessoa *cabecalho  = (CabecalhoPessoa*)malloc(17);
+    fread(&cabecalho->status, sizeof(char), 1 , arq);
+    fread(&cabecalho->quantidadePessoas, sizeof(int), 1 ,arq);
+    fread(&cabecalho->quantidadeRemovidos, sizeof(int), 1, arq);
+    fread(&cabecalho->proxByteOffset, sizeof(long long int), 1, arq);
+    return cabecalho;
+}
+
+void scan_quote_string(char *str)
+{
+
+    /*
+     *	Use essa função para ler um campo string delimitado entre aspas (").
+     *	Chame ela na hora que for ler tal campo. Por exemplo:
+     *
+     *	A entrada está da seguinte forma:
+     *		nomeDoCampo "MARIA DA SILVA"
+     *
+     *	Para ler isso para as strings já alocadas str1 e str2 do seu programa, você faz:
+     *		scanf("%s", str1); // Vai salvar nomeDoCampo em str1
+     *		scan_quote_string(str2); // Vai salvar MARIA DA SILVA em str2 (sem as aspas)
+     *
+     */
+
+    char R;
+
+    while ((R = getchar()) != EOF && isspace(R))
+        ; // ignorar espaços, \r, \n...
+
+    if (R == 'N' || R == 'n')
+    { // campo NULO
+        getchar();
+        getchar();
+        getchar();       // ignorar o "ULO" de NULO.
+        strcpy(str, ""); // copia string vazia
+    }
+    else if (R == '\"')
+    {
+        if (scanf("%[^\"]", str) != 1)
+        { // ler até o fechamento das aspas
+            strcpy(str, "");
+        }
+        getchar(); // ignorar aspas fechando
+    }
+    else if (R != EOF)
+    { // vc tá tentando ler uma string que não tá entre aspas! Fazer leitura normal %s então, pois deve ser algum inteiro ou algo assim...
+        str[0] = R;
+        scanf("%s", &str[1]);
+    }
+    else
+    { // EOF
+        strcpy(str, "");
+    }
+}
--- EDIII_Trabalho1/main.c
+++ EDIII_Trabalho1/main.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "funcionalidades.h"
+#include "auxiliares_busca.h"
+
 
 #define MAXIMO 500
 
 int main()
 {
-
     int funcionalidade;
+    int resultado;
     scanf("%d", &funcionalidade);
     if (funcionalidade == 1)
     {
@@ -20,18 +21,37 @@ int main()
 
     if (funcionalidade == 3)
     {
-        char *arquivo_bin = (char *)malloc(MAXIMO * sizeof(char));
+        //Lê o nome do arquivo binário
+        char arquivo_bin[MAXIMO];
         scanf("%s", arquivo_bin);
-        ler_arquivo_binario(arquivo_bin);
+
+        //Returna 2 se tudo certo, 1 se o não houver registro e 0 se houver erro na abertura do arquivo
+        resultado = ler_arquivo_binario(arquivo_bin);
+
+        if(resultado == 0)
+        {
+            erro_abertura();
+        }
+        if(resultado == 1)
+        {
+            erro_registro();  
+        }
+
     }
     if (funcionalidade == 4)
     {
-        char *arquivo_dados = (char *)malloc(MAXIMO * sizeof(char));
-        char *arquivo_indice = (char *)malloc(MAXIMO * sizeof(char));
+        char arquivo_dados[MAXIMO];
+        char arquivo_indice[MAXIMO];
         int qtd;
         scanf("%s", arquivo_dados);
         scanf("%s", arquivo_indice);
         scanf("%d", &qtd);
-        busca_em_arquivo(arquivo_dados, arquivo_indice, qtd);
+
+        resultado = busca_em_arquivo(arquivo_dados, arquivo_indice, qtd);
+    
+        if(resultado == 0)
+        {
+            erro_abertura();
+        }
     }
 }
